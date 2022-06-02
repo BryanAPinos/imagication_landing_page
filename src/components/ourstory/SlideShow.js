0cbe75b3eddfed9fbe6649691a2d9b0c4ae8a3f7
Diana Rosado <65784827+Diana-Rosado@users.noreply.github.com>
2022-06-02 12:26:08 -0400

    Updating Jest/eslint package

---
+++ src/components/ourstory/SlideShow.js
@@ -0,0 +1,18 @@
+import Image from '@material-tailwind/react/Image';
+import { Typography, Button, Box, Grid, Card, CardContent, CardMedia, Container} from '@mui/material'
+import JFK from  '../../assets/img/JFK.jpg';
+import NJCDC from  '../../assets/img/njcdc.jpg'
+import PS13 from  '../../assets/img/ps13.jpg'
+import Carousel from 'nuka-carousel';
+
+
+
+export default function SlideShow() {
+
+
+    return (
+        <Box className='pb-6'>
+
+        </Box>
+    );
+}
