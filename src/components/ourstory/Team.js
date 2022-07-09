f6166c02ca59863d7800fa36553b3aae189b8e33
Diana Rosado <65784827+Diana-Rosado@users.noreply.github.com>
2022-07-09 18:12:38 -0400

    Removed Nallely

--- src/components/ourstory/Team.js
+++ src/components/ourstory/Team.js
@@ -15,17 +15,17 @@ export default function Team() {
             title: 'CTO & Co-Founder',
             img: Bryan,
         },
-        {
-            name: 'Nallely Inoa',
-            title: 'Graphic Designer',
-            img: Nallely,
-        },
+        // {
+        //     name: 'Nallely Inoa',
+        //     title: 'Graphic Designer',
+        //     img: Nallely,
+        // },
     ]
     
 
     const listTeam = team.map((team,i)=>
-        <Grid container >
-            <Card key ={i} style={{boxShadow: '-5px 3px 11px 1px rgba(0,0,0,0.1)'}}>
+        <Grid container className='flex justify-center' >
+            <Card key ={i} style={{boxShadow: '-5px 3px 11px 1px rgba(0,0,0,0.1)', }}>
                 <CardContent>
                     <Typography variant='subtitle2'>
                         {team.title}
@@ -42,11 +42,11 @@ export default function Team() {
     );
 
     return (
-        <Box className='pb-36 pt-8'>
+        <Box className='pb-36 pt-8' style={{}}>
             <Typography variant ='h2' className='text-center pb-10' style={{fontWeight:'600', color: '#3D325F'}}>
                 Meet the Team
             </Typography>
-            <Container style={{ display: 'flex'}}>
+            <Container  style={{display: 'flex'}}>
                 {listTeam}
             </Container>
         </Box>
