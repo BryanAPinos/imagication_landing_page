4860e3963030c0c7269a7b8eb8db02536083296f
Diana Rosado <65784827+Diana-Rosado@users.noreply.github.com>
2022-07-16 19:16:40 -0400

    removing craig shadow

--- src/components/landing/Navigate.js
+++ src/components/landing/Navigate.js
@@ -1,5 +1,5 @@
 import { Typography, Container, Box, Grid, Button} from '@mui/material'
-import Craig from 'assets/img/Craig 1.png'
+import Craig from 'assets/img/Craig.png'
 import { Link } from "react-router-dom";
 
 
@@ -8,7 +8,7 @@ export default function Navigate() {
         <Box>
             <Grid container direction='column' justifyContent='center' alignItems='center' className='py-12' >
                 <Grid item> 
-                    <img src={Craig} alt='Craig Icon'/>
+                    <img className='w-40' src={Craig} alt='Craig Icon'/>
                 </Grid>
 
                 <Typography variant='h3' className='py-8'>
